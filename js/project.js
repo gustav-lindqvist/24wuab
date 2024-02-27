5a9744a4216b0f2b3f6607626467f327275fbd67
cocacola1 <danielhe108@icloud.com>
2024-02-27 10:36:38 +0100

    Hamburgermeny(mobil)
    Kopierade hamburger menyn från tidigare övningar

--- js/project.js
+++ js/project.js
@@ -1,1 +1,10 @@
-JS goes here... 
+document.querySelector(".menu-button").addEventListener("click", showMenu)
+document.querySelector(".fade-layer").addEventListener("click", showMenu)
+
+function showMenu(){
+    let menu = document.querySelector("nav.menu");
+    menu.classList.toggle("show");
+
+    let layer = document.querySelector(".fade-layer")
+    layer.classList.toggle("visible")
+}
